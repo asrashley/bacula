b5a7f2519dbcc470d7f887b8ab9f9e2281c668b4
Alexis Ashley <github@ashley-family.net>
2025-09-01 09:49:24 +0100

    move config files into web and api directories
    move all of the runtime files for bacula-api and bacula-web into
    an "api" and "web" directory respectively. There is almost certainly
    more work to do, as there appears to be a lot of duplication between
    these two components.

--- docker/first-time-setup.sh
+++ docker/first-time-setup.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 BACULA_GID=$(cat /etc/group | grep ^bacula | cut -d: -f3)
 BACULA_UID=$(cat /etc/passwd | grep ^bacula | cut -d: -f3)
+
 PG_GID=$(cat /etc/group | grep ^postgres | cut -d: -f3)
 if [ -z "${PG_GID}" ]; then
     PG_GID=$(cat /etc/group | grep ^mysql | cut -d: -f3)
 fi
+
 PG_UID=$(cat /etc/passwd | grep ^postgres | cut -d: -f3)
 if [ -z "${PG_UID}" ]; then
     PG_UID=$(cat /etc/passwd | grep ^mysql | cut -d: -f3)
+fi
+
+WWW_GID=$(cat /etc/group | grep ^www-data | cut -d: -f3)
+if [ -z "${WWW_GID}" ]; then
+    WWW_GID=$(cat /etc/group | grep ^nginx | cut -d: -f3)
+fi
+
+WWW_UID=$(cat /etc/passwd | grep ^www-data | cut -d: -f3)
+if [ -z "${WWW_UID}" ]; then
+    WWW_UID=$(cat /etc/passwd | grep ^nginx | cut -d: -f3)
 fi
 
 if [ ! -f .env ]; then
@@ -19,6 +31,7 @@ if [ ! -f .env ]; then
         sudo adduser --system --no-create-home --gid ${BACULA_GID} bacula
         BACULA_GID=$(cat /etc/passwd | grep ^bacula | cut -d: -f3)
     fi
+
     if [ -z "${PG_GID}" ]; then
         sudo addgroup --system postgres
         PG_GID=$(cat /etc/group | grep ^postgres | cut -d: -f3)
@@ -27,6 +40,16 @@ if [ ! -f .env ]; then
         sudo adduser --system --no-create-home --gid ${PG_GID} postgres
         PG_UID=$(cat /etc/passwd | grep ^postgres | cut -d: -f3)
     fi
+
+    if [ -z "${WWW_GID}" ]; then
+        sudo addgroup --system www-data
+        WWW_GID=$(cat /etc/group | grep ^www-data | cut -d: -f3)
+    fi
+    if [ -z "${WWW_UID}" ]; then
+        sudo adduser --system --no-create-home --gid ${PG_GID} www-data
+        PG_UID=$(cat /etc/passwd | grep ^www-data | cut -d: -f3)
+    fi
+
     cat > .env <<EOF
     BACULA_KEY=$1
     BACULA_VERSION=15.0.2
@@ -34,6 +57,8 @@ if [ ! -f .env ]; then
     BACULA_UID=${BACULA_UID}
     PG_GID=${PG_GID}
     PG_UID=${PG_UID}
+    WWW_GID=${WWW_GID}
+    WWW_UID=${WWW_UID}
     EMAIL=${USER}
     DNS_SERVER="8.8.8.8"
     DNS_SEARCH="home.local"
@@ -41,21 +66,44 @@ EOF
 fi
 
 mkdir -p logs/api logs/dir logs/web
-chown -R bacula:bacula logs
+sudo chown -R bacula:bacula logs
+
 mkdir -p working/api working/dir working/fd working/sd working/web
-chown -R bacula:bacula working
+sudo chown -R bacula:bacula working
 
-if [ ! -f config/api/bacularis.users ]; then
+mkdir -p ./api/logs/api ./api/logs/web ./api/config/api ./api/config/web
+mkdir -p ./api/assets ./api/runtime ./api/working ./working/sd
+sudo chown -R ${WWW_UID}:${WWW_GID} ./api/assets ./api/runtime
+
+mkdir -p ./web/logs/api ./web/logs/web ./web/assets ./web/runtime
+mkdir -p ./web/config/web ./web/config/api
+sudo chown -R ${WWW_UID}:${WWW_GID} ./web/assets ./web/logs ./web/runtime
+
+if [ ! -f api/config/api/bacularis.users ]; then
     # username=admin password=admin
-    cat > config/api/bacularis.users <<EOF
+    cat > api/config/api/bacularis.users <<EOF
 admin:$apr1$6hYFTlhE$0vj91PWcNlEjodBYuCEr9/
 EOF
 fi
 
-if [ ! -f config/web/bacularis.users ]; then
+if [ ! -f ./api/config/web/bacularis.users ]; then
     # username=admin password=admin
-    cat > config/web/bacularis.users <<EOF
+    cat > ./api/config/web/bacularis.users <<EOF
 admin:$apr1$6hYFTlhE$0vj91PWcNlEjodBYuCEr9/
 EOF
 fi
-chown -R bacula:bacula config
+sudo chown -R ${WWW_UID}:${WWW_GID} ./api/config
+
+if [ ! -f web/config/api/bacularis.users ]; then
+    cat > web/config/api/bacularis.users <<EOF
+admin:$apr1$6hYFTlhE$0vj91PWcNlEjodBYuCEr9/
+EOF
+fi
+
+if [ ! -f web/config/web/bacularis.users ]; then
+    cat > web/config/web/bacularis.users <<EOF
+admin:$apr1$6hYFTlhE$0vj91PWcNlEjodBYuCEr9/
+EOF
+fi
+
+sudo chown -R ${WWW_UID}:${WWW_GID} ./web/config
