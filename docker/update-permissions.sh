924845ff77a8ef5abf97d58bdf0dffebfdb4afa1
asrashley <alex@ashley-family.net>
2025-09-04 21:30:37 +0100

    all directories in 'api' need to belong to www user
    all of the code in the API service run within php-fpm using the
    www UID

--- docker/update-permissions.sh
+++ docker/update-permissions.sh
@@ -5,8 +5,7 @@ source .env
 set -x
 chown -R bacula:bacula logs
 chown -R bacula:bacula working
-chown -R ${WWW_UID}:${WWW_GID} ./api/assets ./api/runtime
+chown -R ${WWW_UID}:${WWW_GID} ./api
 chown -R ${WWW_UID}:${WWW_GID} ./web/assets ./web/logs ./web/runtime
-chown -R ${WWW_UID}:${WWW_GID} ./api/config
 chown -R ${WWW_UID}:${WWW_GID} ./web/config
 
